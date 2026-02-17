dea9684fd08b2d67042431fdbd8b71143c92f938
cocuum <jonrjolley@gmail.com>
2026-02-17 14:47:11 -0800

    CH3:Player-L5:Groups

--- main.py
+++ main.py
@@ -10,11 +10,19 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    #initiate game
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    
+    #spawn groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
 
+    Player.containers = (updatable,drawable)
+
+    # spawn player
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
 
     while True:
@@ -26,7 +34,9 @@ def main():
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
-        player.update(dt)
+        updatable.update(dt)
+        for drawing in drawable:
+            drawing.draw()
 
 
 if __name__ == "__main__":
