eb02e6458f6269dbe7fbfac122db85cc2e83c7fc
cocuum <jonrjolley@gmail.com>
2026-02-18 19:16:26 -0800

    CH4:Asteoids-L4:RateLimit

--- player.py
+++ player.py
@@ -4,13 +4,15 @@ from constants import (LINE_WIDTH,
                        PLAYER_RADIUS,
                        PLAYER_SHOOT_SPEED,
                        PLAYER_SPEED,
-                       PLAYER_TURN_SPEED,)
+                       PLAYER_TURN_SPEED,
+                       PLAYER_SHOOT_COOLDOWN_SECONDS)
 from shot import Shot
 
 class Player(CircleShape):
     def __init__(self,x,y):
         super().__init__(x,y,PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_cooldown_timer = 0
 
     # in the Player class
     def triangle(self):
@@ -33,10 +35,15 @@ class Player(CircleShape):
         self.position += rotated_speed_vector
 
     def shoot(self):
+        if self.shot_cooldown_timer > 0:
+            return
+        else:
+            self.shot_cooldown_timer = PLAYER_SHOOT_COOLDOWN_SECONDS
         shot = Shot(self.position.x,self.position.y)
         shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
     
     def update(self,dt):
+        self.shot_cooldown_timer -= dt
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
             self.rotate(-dt)
