8f8df567c5380ac923ba0c2198c701870d5c0e56
cocuum <jonrjolley@gmail.com>
2026-02-18 17:23:23 -0800

    fixed asteroid display

--- asteroidfield.py
+++ asteroidfield.py
@@ -1,5 +1,5 @@
-import pygame
 import random
+import pygame
 from asteroid import Asteroid
 from constants import *
 
--- main.py
+++ main.py
@@ -16,34 +16,41 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
     
     #spawn groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    
+    Asteroid.containers = (asteroids,updatable,drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
 
     Player.containers = (updatable,drawable)
-    Asteroid.containers = (asteroids,updatable,drawable)
-    AsteroidField.containers = (updatable)
-    AsteroidField()
 
     # spawn player
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
 
+    dt = 0
+
     while True:
         log_state()
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
-        player.draw(screen)
-        pygame.display.flip()
-        dt = clock.tick(60)/1000
+        
         updatable.update(dt)
+        
+        screen.fill("black")
+        
         for drawing in drawable:
             drawing.draw(screen)
 
+        pygame.display.flip()
+
+        #limit the framerate to 60FPS
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
