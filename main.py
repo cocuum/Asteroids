6136cdd25ce08691b7c902238019ee65d121e1a3
cocuum <jonrjolley@gmail.com>
2026-02-18 16:55:10 -0800

    CH4:Asteroids-L1:Asteroids

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import SCREEN_WIDTH,SCREEN_HEIGHT
 from logger import log_state
 from player import Player
@@ -19,8 +21,12 @@ def main():
     #spawn groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable,drawable)
+    Asteroid.containers = (asteroids,updatable,drawable)
+    AsteroidField.containers = (updatable)
+    AsteroidField()
 
     # spawn player
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
